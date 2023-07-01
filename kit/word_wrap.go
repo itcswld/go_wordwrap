930d35a8a3677dca7590f8694d3528834f328460
itcswld <itcswld@gmail.com>
2023-07-02 00:47:01 +0800

    wordwarp del print

--- kit/word_wrap.go
+++ kit/word_wrap.go
@@ -1,20 +1,15 @@
 package kit
 
-import "fmt"
-
-// "strconv"
-
-func SliceStr(s string, n int) (arr_str []string) {
-	txtRune := []rune(s)
-	for i := 1; i < len(txtRune); i++ {
-		if i%n == 0 {
+func SliceStr(txt []rune, breakLen int) (arr_str []string) {
+	for i := 1; i < len(txt); i++ {
+		if i%breakLen == 0 {
 			//slicing
-			arr_str = append(arr_str, string(txtRune[:i])) //[0][1]
-			txtRune = txtRune[i:]                          //[i]~[len(s)]
+			arr_str = append(arr_str, string(txt[:i])) //[0][1]
+			txt = txt[i:]                              //[i]~[len(s)]
 			i = 1
 		}
 	}
-	arr_str = append(arr_str, string(txtRune[:]))
+	arr_str = append(arr_str, string(txt[:]))
 	return arr_str
 }
 
@@ -23,21 +18,16 @@ func WordWrap(text []rune, breakLen int) (data []string) {
 	pre := 0
 out:
 	for j, v := range text {
-		fmt.Println("new:", string(text[:]), "| len= ", len(text))
 		if IsChinese(v) {
 			n += 1
 		} else {
 			n += 0.5
 		}
 		if len(text) <= breakLen || (j-pre == len(text)-1 && n < float64(breakLen)) {
-			fmt.Println("j=", j, "n =", n)
 			data = append(data, string(text[:]))
-			fmt.Println("last:", string(text[:]))
 			break out
 		} else if len(text) > breakLen && n >= float64(breakLen) {
-			fmt.Println("j=", j, ";n =", n, ";t =", pre, ";j-t=", j-pre)
 			data = append(data, string(text[:j-pre]))
-			fmt.Println("cut:", string(text[:j-pre]))
 			text = text[j-pre:]
 			pre += j - pre
 			n = 0.0
